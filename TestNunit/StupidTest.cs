121994215f91a6d477625151882173553dd603f0
Gian Maria Ricci <alkampfer@nablasoft.com>
2021-02-05 09:30:49 +0100

    First commit with a test.

---
+++ TestNunit/StupidTest.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestNunit
+{
+    [TestFixture]
+    public class StupidTest
+    {
+        [TestCase(',')]
+        public void Verify_re_run(char parameter) 
+        {
+            Assert.Fail("This test will always fails");
+        }
+    }
+}
