78d401edca131f9e9df1246c1eb4d4e0131241a1
Gian Maria <ricci.gianmaria@gmail.com>
2021-02-05 09:33:20 +0100

    First pipeline test.
    added build and some more tests

--- TestNunit/StupidTest.cs
+++ TestNunit/StupidTest.cs
@@ -11,9 +11,22 @@ namespace TestNunit
     public class StupidTest
     {
         [TestCase(',')]
-        public void Verify_re_run(char parameter) 
+        [TestCase('1')]
+        [TestCase('2')]
+        [TestCase('3')]
+        [TestCase('4')]
+        [TestCase('5')]
+        [TestCase('6')]
+        [TestCase('7')]
+        [TestCase('8')]
+        [TestCase('9')]
+        [TestCase('0')]
+        public void Verify_re_run(char parameter)
         {
-            Assert.Fail("This test will always fails");
+            if (parameter == ',')
+            {
+                Assert.Fail("This test will always fails with ,");
+            }
         }
     }
 }
